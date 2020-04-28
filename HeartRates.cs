5644eee222fa34bddec5111854c9049a19403db5
OrigamiOctopus <54693758+OrigamiOctopus@users.noreply.github.com>
2020-04-28 14:22:06 -0700

    Add files via upload

---
+++ HeartRates.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthApp
+{
+    class HeartRates
+    {
+        private string firstName;
+        private string lastName;
+        private int yearOfBirth;
+        private int currentYear;
+        private int age;
+        private int maxHeartRate;
+        private int minTarget;
+        private int maxTarget;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public int YearOfBirth
+        {
+            get
+            {
+                return yearOfBirth;
+            }
+            set
+            {
+                yearOfBirth = value;
+            }
+        }
+        public int CurrentYear
+        {
+            get
+            {
+                return currentYear;
+            }
+            set
+            {
+                currentYear = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public int FindAge()
+        {
+            age = currentYear - yearOfBirth;
+            return age;
+        }
+        public int MaxHeartRate()
+        {
+            maxHeartRate = 220 - age;
+            return maxHeartRate; 
+        }
+        public int MinTargetHeartRate()
+        {
+            minTarget = Convert.ToInt32(maxHeartRate * .50);
+            return minTarget;
+        }
+        public int MaxTargetHeartRate()
+        {
+            maxTarget = Convert.ToInt32(maxHeartRate * .85);
+            return maxTarget;
+        }
+
+        public HeartRates(string fName, string lName, int yOBirth, int cYear)
+        {
+            FirstName = fName;
+            LastName = lName;
+            YearOfBirth = yOBirth;
+            CurrentYear = cYear;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HealthApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first;
+            string last;
+            int birth;
+            int curYear;
+           
+
+            Console.WriteLine("Please enter first name.");
+            first = Console.ReadLine();
+            Console.WriteLine("Please enter last name.");
+            last = Console.ReadLine();
+            Console.WriteLine("Please enter birth year (ie 1986).");
+            birth = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the current year (ie 2019).");
+            curYear = Int32.Parse(Console.ReadLine());
+
+            HeartRates firstPerson = new HeartRates(first, last, birth, curYear);
+
+            Console.WriteLine($"\n{firstPerson.FirstName} {firstPerson.LastName} is {firstPerson.FindAge()}," +
+                $" has a maximum heart rate of {firstPerson.MaxHeartRate()}," +
+                $" a minimum target heart rate of {firstPerson.MinTargetHeartRate()}," +
+                $" and a maximum target heart rate of {firstPerson.MaxTargetHeartRate()}.");
+            
+
+
+        }
+    }
+}
